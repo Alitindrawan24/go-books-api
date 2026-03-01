955e61b3e92f0facf734b6ac1eb96bd58e7d0823
Alit Indrawan <alitindrawan71@gmail.com>
2026-03-01 20:49:52 +0800

    refactor(repository): improve in-memory book storage and retrieval

--- internal/repository/memory/books/books.go
+++ internal/repository/memory/books/books.go
@@ -6,46 +6,28 @@ import (
 	"github.com/Alitindrawan24/go-books-api/internal/entity"
 )
 
+var books []entity.Book
+
 func (repository *Repository) FindBooks(ctx context.Context) ([]entity.Book, error) {
-
-	books := []entity.Book{
-		{
-			ID:     1,
-			Title:  "Book Title",
-			Author: "Book Author",
-			Year:   2023,
-		},
-		{
-			ID:     2,
-			Title:  "Book Title",
-			Author: "Book Author",
-			Year:   2023,
-		},
-		{
-			ID:     3,
-			Title:  "Book Title",
-			Author: "Book Author",
-			Year:   2023,
-		},
-	}
-
 	return books, nil
 }
 
 func (repository *Repository) FindBookByID(ctx context.Context, id int) (entity.Book, error) {
-	return entity.Book{
-		ID:     id,
-		Title:  "Book Title",
-		Author: "Book Author",
-		Year:   2023,
-	}, nil
+	book := entity.Book{}
+	for _, b := range books {
+		if b.ID == id {
+			book = b
+			break
+		}
+	}
+
+	return book, nil
 }
 
 func (repository *Repository) InsertBook(ctx context.Context, book entity.Book) (entity.Book, error) {
-	return entity.Book{
-		ID:     1,
-		Title:  book.Title,
-		Author: book.Author,
-		Year:   book.Year,
-	}, nil
+	book.ID = len(books) + 1
+
+	books = append(books, book)
+
+	return book, nil
 }
